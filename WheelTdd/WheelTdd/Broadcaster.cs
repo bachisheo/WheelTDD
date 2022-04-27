1e9cba7b8c35217b54b9bf25705b239bef458761
olga <bachisheo@gmail.com>
2022-04-27 20:07:11 +0700

    Реализовано приветсвие игроков ведущим игры

---
+++ WheelTdd/WheelTdd/Broadcaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelTdd
+{
+    /// <summary>
+    /// Класс, реализующий функционал ведущего игры
+    /// </summary>
+    public class Broadcaster
+    {
+        
+        /// <summary>
+        /// Приветсвие игрока ведущим игры
+        /// </summary>
+        /// <returns>Текст приветствия</returns>
+        public string SayGreeting()
+        {
+            //todo сделать вывод правил игры
+            return "Приветствую вас на игре \"Поле чудес\"!";
+        }
+    }
+}
