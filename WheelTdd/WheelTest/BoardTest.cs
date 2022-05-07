b207ea72934ec620ebfcccf478d4b48c4b513a67
olga <bachisheo@gmail.com>
2022-05-07 10:44:57 +0700

    Добавлен текст-класс для класса "Доска". Добавлен тест инициализации доски.

---
+++ WheelTdd/WheelTest/BoardTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WheelTest
+{
+    [TestClass]
+    public class BoardTest
+    {
+        /// <summary>
+        /// Проверка корректности начального состояния табло
+        /// для заданного слова
+        /// </summary>
+        [TestMethod]
+        public void TestWordMask()
+        {
+            string word = "somestring";
+            Board board = new Board(word);
+            string boardState = board.State;
+            Assert.AreEqual(boardState.Length, word.Length);
+            for (int i = 0; i < word.Length; i++)
+            {
+                Assert.AreEqual(boardState[i], Board.SecretSign);
+            }
+        }
+
+    }
+}
