db11ebb6fd3d1312be58139acc2a837828d1dde0
olga <bachisheo@gmail.com>
2022-05-08 11:04:31 +0700

    Добавлена проверка методов для проверки открытия одной буквы/ всего слова. Изменен предыдущий метод (теперь тестируется на наборе слов).

--- WheelTdd/WheelTest/BoardTest.cs
+++ WheelTdd/WheelTest/BoardTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WheelTdd;
 
@@ -7,6 +9,10 @@ namespace WheelTest
     [TestClass]
     public class BoardTest
     {
+        private string[] GetWords()
+        {
+            return new[] { "somestring", "other-string" };
+        }
         /// <summary>
         /// Проверка корректности начального состояния табло
         /// для заданного слова
@@ -14,13 +20,45 @@ namespace WheelTest
         [TestMethod]
         public void TestWordMask()
         {
-            string word = "somestring";
-            Board board = new Board(word);
-            string boardState = board.State;
-            Assert.AreEqual(boardState.Length, word.Length);
-            for (int i = 0; i < word.Length; i++)
+            foreach (var word in GetWords())
             {
-                Assert.AreEqual(boardState[i], Board.SecretSign);
+                Board board = new Board(word);
+                string boardState = board.State;
+                Assert.AreEqual(boardState.Length, word.Length);
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Assert.AreEqual(boardState[i], Board.SecretSign);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestLettersOpening()
+        {
+            foreach (var word in GetWords())
+            {
+                var usedLetter = new HashSet<char>();
+                var board = new Board(word);
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (!usedLetter.Contains(word[i]))
+                    {
+                        Assert.AreEqual(board.State[i], Board.SecretSign);
+                        board.OpenLetter(word[i]);
+                        usedLetter.Add(word[i]);
+                    }
+                    Assert.AreEqual(board.State[i], word[i]);
+                }
+            }
+        }        
+        [TestMethod]
+        public void TestWordOpening()
+        {
+            foreach (var word in GetWords())
+            {
+                var board = new Board(word);
+                board.OpenWord();
+                Assert.AreEqual(word, board.State);
             }
         }
 
