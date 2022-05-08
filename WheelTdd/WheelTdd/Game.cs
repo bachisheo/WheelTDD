8a77204f1b3c1e3fd8b991a6ff5bcc421f4572c2
olga <bachisheo@gmail.com>
2022-05-08 23:17:04 +0700

    Реализована метод, возвращающий, все ли буквы открыты

--- WheelTdd/WheelTdd/Game.cs
+++ WheelTdd/WheelTdd/Game.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace WheelTdd
 {
     public class Game
     {
         private string _word;
-
         public Game(string word)
         {
             _word = word;
             Board = new Board(_word);
         }
-        public Board Board { get; private set; }
-        public HashSet<char> UsedLetters { get; private set; } = new();
+        public Board Board { get; }
+        public HashSet<char> UsedLetters { get; } = new();
         public int CheckUserAnswer(char c)
         {
             if (!_word.Contains(c) || UsedLetters.Contains(c))
@@ -25,5 +25,14 @@ namespace WheelTdd
                 return 0;
             return Board.OpenWord();
         }
+
+        public bool IsAllOpen()
+        {
+            foreach(var s in Board.State)
+                if (s == Board.SecretSign)
+                    return false;
+            return true;
+        }
+
     }
 }
