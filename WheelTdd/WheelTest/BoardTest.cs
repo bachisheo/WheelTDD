83560afad8c91ccf39d2fd39f66a38eba78d013d
olga <bachisheo@gmail.com>
2022-05-08 11:26:58 +0700

    Добавлены тесты для проверки количества открытых букв, возвращаемых после открытия буквы или слова.

--- WheelTdd/WheelTest/BoardTest.cs
+++ WheelTdd/WheelTest/BoardTest.cs
@@ -61,6 +61,62 @@ namespace WheelTest
                 Assert.AreEqual(word, board.State);
             }
         }
+        /// <summary>
+        /// Проверка корректности возвращаемого методом количества
+        /// открытых букв
+        /// </summary>
+        [TestMethod]
+        public void TestLettersOpeningCount()
+        {
+            foreach (var word in GetWords())
+            {
+                var usedLetter = new HashSet<char>();
+                var board = new Board(word);
+                var lettersCount = PreCountWordStructure(word);
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (!usedLetter.Contains(word[i]))
+                    {
+                        Assert.AreEqual(board.OpenLetter(word[i]), lettersCount[word[i]]);
+                        usedLetter.Add(word[i]);
+                    }
+                    Assert.AreEqual(board.OpenLetter(word[i]), 0);
+                }
+            }
+        }
+
+        private Dictionary<char, int> PreCountWordStructure(string word)
+        {
+            var letters = new Dictionary<char, int>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                if(letters.ContainsKey(word[i]))
+                    letters[word[i]]++;
+                else
+                {
+                    letters.Add(word[i], 1);
+                }
+            }
+            return letters;
+        }
+        /// <summary>
+        /// Проверка метода, открывающего слово в случае,
+        /// когда часть букв уже была открыта
+        /// </summary>
+        [TestMethod]
+        public void TestWordOpeningPartially()
+        {
+            foreach (var word in GetWords())
+            {
+                foreach (var letter in PreCountWordStructure(word))
+                {
+                    var board = new Board(word);
+                    board.OpenLetter(letter.Key);
+                    Assert.AreEqual(board.OpenWord(), word.Length - letter.Value);
+                    Assert.AreEqual(word, board.State);
+                }
+            }
+        }
 
     }
 }
