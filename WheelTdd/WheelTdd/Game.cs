e7df5f8b015414604f2a10983f1c1fc2482f2b91
olga <bachisheo@gmail.com>
2022-05-08 17:36:50 +0700

    Реализованы методы проверки ответа

--- WheelTdd/WheelTdd/Game.cs
+++ WheelTdd/WheelTdd/Game.cs
@@ -1,16 +1,29 @@
-﻿namespace WheelTdd
+﻿using System.Collections.Generic;
+
+namespace WheelTdd
 {
     public class Game
     {
-        public Game(string quest){}
+        private string _word;
 
+        public Game(string word)
+        {
+            _word = word;
+            Board = new Board(_word);
+        }
+        public Board Board { get; private set; }
+        public HashSet<char> UsedLetters { get; private set; } = new();
         public int CheckUserAnswer(char c)
         {
-            throw new System.NotImplementedException();
+            if (!_word.Contains(c) || UsedLetters.Contains(c))
+                return 0;
+            return Board.OpenLetter(c);
         }
         public int CheckUserAnswer(string word)
         {
-            throw new System.NotImplementedException();
+            if (word != _word)
+                return 0;
+            return Board.OpenWord();
         }
     }
 }
